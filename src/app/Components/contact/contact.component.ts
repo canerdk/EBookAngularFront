c2b1cd7dd969c76d9bef72bc211f549ea37b6228
canerdk <canerdk1995@hotmail.com>
2021-07-05 01:04:38 +0300

    edited contact form

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ValidationErrors } from '@angular/forms';
+import { FormBuilder, ValidationErrors } from '@angular/forms';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ToastrService } from 'ngx-toastr';
@@ -11,26 +11,30 @@ import { ProviderService } from 'src/app/Services/provider.service';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  msg = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
-    message: new FormControl('', Validators.required)
-  });
+  contactForm!: FormGroup;
+  submitted = false;
 
   constructor( 
     private service: ProviderService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private fb: FormBuilder
     ) { }
 
   ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+      message: new FormControl('', Validators.required)
+    });
   }
 
   sendMessage(){
-    if(this.msg.valid){
-      this.service.postContact(Object.assign({}, this.msg.value)).then((data) => {
+    this.submitted = true;
+    if(this.contactForm.valid){
+      this.service.postContact(Object.assign({}, this.contactForm.value)).then((data) => {
         this.showSuccess();   
-        this.msg.reset();
+        this.contactForm.reset();
       });
     }
     else{
@@ -38,10 +42,15 @@ export class ContactComponent implements OnInit {
       this.showError();      
     }
   }
+
+  get contactFormControl() {
+    return this.contactForm.controls;
+  }
+
   getFormValidationErrors() {
-    Object.keys(this.msg.controls).forEach(key => {
+    Object.keys(this.contactForm.controls).forEach(key => {
   
-    const controlErrors = this.msg.get(key)?.errors;
+    const controlErrors = this.contactForm.get(key)?.errors;
     if (controlErrors != null) {
           Object.keys(controlErrors).forEach(keyError => {
             console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
